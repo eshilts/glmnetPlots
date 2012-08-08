ce82f779115e8bd6a295fa276c0d16ab8da73c28
erik 'quantitative warlock' shilts <erik.shilts@opower.com>
2012-08-08 11:13:54 -0400

    caseykelp --> glmnetplots

--- R/glmnetPlots-package.R
+++ R/glmnetPlots-package.R
@@ -1,4 +1,4 @@
-#' A package for plotting glmnet objects.
+#' glmnetPlots
 #' 
 #' @description This package duplicates the functionality of plot.glmnet and plot.cv.glmnet from
 #' the glmnet package. It extends functionality by adding coefficient plots (a la the arm package)
@@ -6,7 +6,8 @@
 #' having different functionality for cv.glmnet and glmnet objects. This is because it makes the
 #' code easier to parse by allowing the user to explictly state the type of plot they want, and to eliminate
 #' the storing of multiple objects since the glmnet object is embedded in the cv.glmnet object.
-#' @name CaseyKelp
-#' @aliases CaseyKelp dreamysmurf package-dreamysmurf package-CaseyKelp
+#'
+#' @name glmnetPlots
+#' @aliases glmnetPlots package-glmnetPlots
 #' @docType package
 NULL
